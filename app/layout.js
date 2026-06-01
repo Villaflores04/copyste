09c8c7dbf0a409e7558a8521eb4b3bb1c84748ac
Villaflores04 <tkpaulvincent042005@gmail.com>
2026-06-01 21:51:57 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
+import "./globals.css";
+
 export const metadata = {
   title: "CopySte",
-  description: "Copy and edit text panels"
+  description: "Copy and edit text panels",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head />
       <body>
         {children}
       </body>
